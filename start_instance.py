09538fb418e5b4a5569e70538b806a02d95a0304
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-04-09 22:01:43 -0500

    Update start_instance.py

--- start_instance.py
+++ start_instance.py
@@ -1,1 +1,13 @@
 
+import boto3
+
+INSTANCE_ID = 'AWS INSTANCE ID'
+REGION = 'AWS REGION'  # Change if your instance is in another region
+
+def start_instance():
+    ec2 = boto3.client('ec2', region_name=REGION)
+    ec2.start_instances(InstanceIds=[INSTANCE_ID])
+    print(f"Instance {INSTANCE_ID} started.")
+
+if __name__ == "__main__":
+    start_instance()
